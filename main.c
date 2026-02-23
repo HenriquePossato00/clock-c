bf7b8f6a34099dbe37078bea68fcba259a663cd3
Henrique Rocha Possato <hpossato00@gmail.com>
2026-02-23 20:10:59 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    // This is my first program!
+
+    /*
+    This 
+    is
+    also
+    a 
+    comment
+    */
+
+    printf("I like Pizza!\n");
+    printf("It's really good!");
+    
+    return 0;
+}
